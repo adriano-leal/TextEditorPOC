02d8d6222e8bb6fb1fce467ef2c61a70c74bafa0
Adriano Leal <adriano.ramos@sap.com>
2023-01-24 16:19:26 -0300

    Initial Commit

---
+++ TextEditorPOC/TextEditorPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorPOCApp.swift
+//  TextEditorPOC
+//
+//  Created by Adriano Leal on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
